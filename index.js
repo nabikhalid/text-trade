a77ae87f81690df0d241c53a16ba21634326b24c
Nabi <nabi@ecocarrier.com>
2020-10-18 15:28:55 -0400

    worked on app

--- index.js
+++ index.js
@@ -14,120 +14,168 @@ app.use(express.json());
 //     res.sendFile(path.join(__dirname, '/index.html'));
 // });
 
-// var allText = new List();
+// logic modelling
 
-var allText = [
-    {id: 1, owner: 'NK', text: 'Introduction to Node JS: 5th Edition'}
+var courses = [
+    {id: '', grade: '', credits: 0.0}
 ];
 
-var allUser = [
-    {email: 'NK@gmail.com', password: 'password', username: 'NK', myText: []}
-];
+// can change type of scale, 12-point, 4-point, etc.
 
-// let courses = [
-//     {id: 1, name: 'math'}, 
-//     {id: 2, name: 'english'}, 
-//     {id: 3, name: 'physical activity'}
-// ];
+function calculateGPA(courses) {
+    var totalPoints, totalCredits;
+    for (i=0;i<courses.length;i++) {
+        totalPoints = totalPoints + gradeToPoints(courses[i].grade) * courses[i].credits;
+        totalCredits = totalCredits + courses[i].credits;
+    }
+    return totalPoints / totalCredits;
+}
+
+function gradeToPoints(letterGrade){
+    if(letterGrade === 'A+') {
+        return 9.0;
+    }
+    else if (letterGrade === 'A'){
+        return 8.0
+    }
+    else if (letterGrade === 'B+'){
+        return 7.0;
+    }
+    else if (letterGrade === 'B'){
+        return 6.0;
+    }
+    else if (letterGrade === 'C+'){
+        return 5.0;
+    }
+    else if (letterGrade === 'C'){
+        return 4.0;
+    }
+    else if (letterGrade === 'D+'){
+        return 3.0;
+    }
+    else if (letterGrade === 'D'){
+        return 2.0;
+    }
+    else if (letterGrade === 'E'){
+        return 1.0;
+    }
+    else if (letterGrade === 'F'){
+        return 0.0;
+    }
+    else {
+        return -1.0;
+    }
+}
+
+function validGrade(grade) {
+    if(grade === 'A+' || grade === 'A' || grade === 'B+' || grade === 'B' || grade === 'C+' || grade === 'C' || grade === 'D+' || grade === 'D' || grade === 'E' || grade === 'F') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+function validCredits(credits){
+    if(credits === 0,1,2,3,6){
+        return true;
+    }
+    return false;
+}
 
 // '/' is the root of a website
 // this is a route to root
 app.get('/', (req, res) => {
-    res.send('Hello, welcome to Text Trade.');
+    res.send('Hello, welcome to your GPA Calculator.');
 });
 
-app.post('/register', (req, res) => {
+app.post('/input', (req, res) => {
     // input validation
-    if(!req.body.email || !req.body.password || !req.body.username){
-        res.status(400).send('One of your email, password, or username is missing.');
+    if(!req.body.id || !req.body.grade || !req.body.credits){
+        res.status(400).send('One of your course ID, grade, or credits is missing.');
         return;
     }
-    let username = allUser.find(u => u.username === req.params.username);
-    if (!username) {
-        let user = {
-            email: req.body.email, 
-            password: req.body.password, 
+    if (!validGrade(req.body.grade) || !validCredits(req.body.credits)){
+        res.status(400).send('One of your course grade or credits is invalid.');
+        return;
+    }
+    let course = courses.find(c => c.id === req.params.id);
+    if (!course) {
+        let newCourse = {
+            id: req.body.id, 
+            grade: req.body.grade, 
             // parse JSON
-            username: req.body.username,
-            myText: []
+            credits: parseInt(req.body.credits),
         };
     
-        allUser.push(user);
-        res.send(user);
+        courses.push(newCourse);
+        res.send(newCourse);
     }
-    res.status(400).send('Username is already taken.');
-    return;
+    res.status(400).send('Course is already in database.');
 });
 
-app.get('/allText', (req, res) => {
-    res.send(allText);
+app.get('/getgpa', (req, res) => {
+    res.send(calculateGPA(courses));
 });
 
-app.get('/allText/:id', (req, res) => {
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
     // backend logic
-    let text = allText.find(t => t.id === parseInt(req.params.id));
+    let course = courses.find(c => c.id === req.params.id);
     // 404 not found
-    if (!text) {
-        res.status(404).send('The text with the given ID was not found.');
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found.');
         return;
     }
-
-    res.send(allText);
+    res.send(courses);
     return;
 });
 
 // can use const if course obj is not reset later
 // can use let otherwise 
 
-app.post('/allText', (req, res) => {
-
-    // input validation
-    if (!req.body.name || req.body.name.length < 3) {
-        // 400 bad request
-        res.status(400).send('Name is required and should be minimum 3 characters.');
-        return;
-    }
-
-    let text = {
-        id: allText.length + 1, 
-        owner: '', 
-        // parse JSON
-        text: req.body.text
-    };
-
-    allText.push(text);
-    res.send(text);
-});
 
 // put 
 // finish this, update for text, not courses
 
-app.put('/allText/:id', (req, res) => {
+app.put('/courses/:id', (req, res) => {
     // find the course, if doesn't exist, 404
-    let text = allText.find(t => t.id === parseInt(req.params.id));
+    let course = courses.find(c => c.id === req.params.id);
     // 404 not found
-    if (!text) res.status(404).send('The text with the given ID was not found.');
-
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found.');
+        return;
+    }
     // bad request 400
-    if (!req.body.id || req.body.text == null) {
-        // 400 bad request
-        res.status(400).send('Invalid text input.');
+    if(!req.body.id || !req.body.grade || !req.body.credits){
+        res.status(400).send('One of your course ID, grade, or credits is missing.');
+        return;
+    }
+    if (!validGrade(req.body.grade) || !validCredits(req.body.credits)){
+        res.status(400).send('One of your course grade or credits is invalid.');
         return;
     }
 
-    text.name = req.body.name;
-    res.send(text);
-
+    course.grade = req.body.grade;
+    course.credits = parseInt(req.body.credits);
+    res.send(course);
 });
 
-app.delete('/allText/:id', (req, res) => {
-    let text = allText.find(t => t.id === parseInt(req.params.id));
-    if (!text) res.status(404).send('The text with the given ID was not found.');
+app.delete('/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === req.params.id);
+    // 404 not found
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found.');
+        return;
+    }
 
-    const index = allText.indexOf(text);
-    allText.splice(index, 1);
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
 
-    res.send(text);
+    res.send(courses);
 });
 
 // environment variable, if port 3000 is not available, for deployment
---
+++ old files/oldindex.js
@@ -0,0 +1,134 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+// var List = require("collections/list");
+
+app.use(express.json());
+
+// app.get();
+// app.post();
+// app.put();
+// app.delete();
+
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// });
+
+// var allText = new List();
+
+var allText = [
+    {id: 1, owner: 'NK', text: 'Introduction to Node JS: 5th Edition'}
+];
+
+var allUser = [
+    {email: 'NK@gmail.com', password: 'password', username: 'NK', myText: []}
+];
+
+// let courses = [
+//     {id: 1, name: 'math'}, 
+//     {id: 2, name: 'english'}, 
+//     {id: 3, name: 'physical activity'}
+// ];
+
+// '/' is the root of a website
+// this is a route to root
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to your GPA Calculator.');
+});
+
+app.post('/register', (req, res) => {
+    // input validation
+    if(!req.body.email || !req.body.password || !req.body.username){
+        res.status(400).send('One of your email, password, or username is missing.');
+        return;
+    }
+    let username = allUser.find(u => u.username === req.params.username);
+    if (!username) {
+        let user = {
+            email: req.body.email, 
+            password: req.body.password, 
+            // parse JSON
+            username: req.body.username,
+            myText: []
+        };
+    
+        allUser.push(user);
+        res.send(user);
+    }
+    res.status(400).send('Username is already taken.');
+    return;
+});
+
+app.get('/allText', (req, res) => {
+    res.send(allText);
+});
+
+app.get('/allText/:id', (req, res) => {
+    // backend logic
+    let text = allText.find(t => t.id === parseInt(req.params.id));
+    // 404 not found
+    if (!text) {
+        res.status(404).send('The text with the given ID was not found.');
+        return;
+    }
+
+    res.send(allText);
+    return;
+});
+
+// can use const if course obj is not reset later
+// can use let otherwise 
+
+app.post('/allText', (req, res) => {
+
+    // input validation
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 bad request
+        res.status(400).send('Name is required and should be minimum 3 characters.');
+        return;
+    }
+
+    let text = {
+        id: allText.length + 1, 
+        owner: '', 
+        // parse JSON
+        text: req.body.text
+    };
+
+    allText.push(text);
+    res.send(text);
+});
+
+// put 
+// finish this, update for text, not courses
+
+app.put('/allText/:id', (req, res) => {
+    // find the course, if doesn't exist, 404
+    let text = allText.find(t => t.id === parseInt(req.params.id));
+    // 404 not found
+    if (!text) res.status(404).send('The text with the given ID was not found.');
+
+    // bad request 400
+    if (!req.body.id || req.body.text == null) {
+        // 400 bad request
+        res.status(400).send('Invalid text input.');
+        return;
+    }
+
+    text.name = req.body.name;
+    res.send(text);
+
+});
+
+app.delete('/allText/:id', (req, res) => {
+    let text = allText.find(t => t.id === parseInt(req.params.id));
+    if (!text) res.status(404).send('The text with the given ID was not found.');
+
+    const index = allText.indexOf(text);
+    allText.splice(index, 1);
+
+    res.send(text);
+});
+
+// environment variable, if port 3000 is not available, for deployment
+app.listen(3000, () => console.log('Listening on port 3000...'));
